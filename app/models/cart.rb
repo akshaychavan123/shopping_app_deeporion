70b067ccfb7a8fa2f9d12cf8c2985eb21ceee2ad
manojverma12345 <manoj.verma@deeporion.com>
2024-06-11 14:14:55 +0530

    remove cart file

--- app/models/cart.rb
+++
@@ -1,7 +0,0 @@
-class Cart < ApplicationRecord
-  belongs_to :user
-  has_many :cart_items, dependent: :destroy
-  has_many :product_items, through: :cart_items
-
-  validates :user_id, presence: true
-end
--- app/models/cart_item.rb
+++
@@ -1,6 +0,0 @@
-class CartItem < ApplicationRecord
-  belongs_to :cart
-  belongs_to :product_item
-
-  validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 1 }
-end
