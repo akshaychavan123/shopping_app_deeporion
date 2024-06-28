5572664b4e92327c2dad737a1dd1321aa1fa5c95
manojverma12345 <manoj.verma@deeporion.com>
2024-06-28 17:41:24 +0530

    resolve bug

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,7 @@
 //= link_tree ../images
+
 //= link_directory ../stylesheets .css
-//= link_tree ../javascripts .js
-//= link_tree ../../../javascript .js
+
+//= link_directory ../javascripts .js
+
+//= link_directory ../../javascript .js
