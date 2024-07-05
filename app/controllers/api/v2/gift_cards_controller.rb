38688a9bc12470374d00999bbd8d2977af1f22af
manojverma12345 <manoj.verma@deeporion.com>
2024-07-05 13:15:32 +0530

    work on gift card admin apis

--- app/controllers/api/v2/gift_cards_controller.rb
+++ app/controllers/api/v2/gift_cards_controller.rb
@@ -4,26 +4,39 @@ class Api::V2::GiftCardsController < ApplicationController
 
   def index
     @gift_cards = GiftCard.all
-    render json: @gift_cards
+    render json: { data: ActiveModelSerializers::SerializableResource.new(@gift_cards, each_serializer: GiftCardSerializer)}
   end
 
   def show
-    render json: @gift_card
+    render json: { data: ActiveModelSerializers::SerializableResource.new(@gift_card, each_serializer: GiftCardSerializer)}
   end
 
   def create
     @gift_card = GiftCard.new(gift_card_params)
 
+    if params[:images].present?
+      Array(params[:images]).each do |image|
+        @gift_card.images.attach(image)
+      end
+    end
+
     if @gift_card.save
-      render json: @gift_card, status: :created
+      render json: { data: ActiveModelSerializers::SerializableResource.new(@gift_card, each_serializer: GiftCardSerializer)}
     else
       render json: @gift_card.errors, status: :unprocessable_entity
     end
   end
 
-  def update
+  def update  
+    if params[:images].present?
+      @gift_card.images.attachments.each(&:purge) if params[:replace_images] == 'true'
+      Array(params[:images]).each do |image|
+        @gift_card.images.attach(image)
+      end
+    end
+  
     if @gift_card.update(gift_card_params)
-      render json: @gift_card
+      render json: { data: ActiveModelSerializers::SerializableResource.new(@gift_card, each_serializer: GiftCardSerializer) }
     else
       render json: @gift_card.errors, status: :unprocessable_entity
     end
@@ -41,7 +54,6 @@ class Api::V2::GiftCardsController < ApplicationController
   end
 
   def gift_card_params
-    params.require(:gift_card).permit(:image, :price, :gift_card_category_id)
+    params.permit(:gift_card_category_id, :price, images: [])
   end
 end
-  
