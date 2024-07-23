1a63cbcae42b2189a7a5de03a3f402cc392e9096
manojverma12345 <manoj.verma@deeporion.com>
2024-07-23 18:01:26 +0530

    work on apis

--- app/serializers/product_item_serializer.rb
+++ app/serializers/product_item_serializer.rb
@@ -1,4 +1,4 @@
 class ProductItemSerializer < ActiveModel::Serializer
-  attributes :name, :brand, :description, :material, :care, :product_code, :product_id
+  attributes :id, :name, :brand, :description, :material, :care, :product_code, :product_id
   has_many :product_item_variants
 end
