888804be15a037b78c1b6974acf542ec3dae75a2
manojverma12345 <manoj.verma@deeporion.com>
2024-07-25 11:14:37 +0530

    add validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :subcategory
   has_many :product_items
-  validates :name, presence: true
   has_many :coupons, as: :couponable
+  validates :name, presence: true, uniqueness: { scope: :subcategory_id }
 end
--- app/models/subcategory.rb
+++ app/models/subcategory.rb
@@ -2,4 +2,5 @@ class Subcategory < ApplicationRecord
   belongs_to :category
   has_many :products, dependent: :destroy
   has_many :coupons, as: :couponable
+  validates :name, presence: true, uniqueness: { scope: :category_id }
 end
