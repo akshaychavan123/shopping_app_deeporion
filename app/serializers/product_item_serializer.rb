142225d22297a57cc595507e39b4828f43039302
SradhaBhavsar <shradha.bhavsar@deeporion.com>
2024-09-11 12:23:14 +0530

    product api code update

--- app/serializers/product_item_serializer.rb
+++ app/serializers/product_item_serializer.rb
@@ -12,7 +12,7 @@ class ProductItemSerializer < ActiveModel::Serializer
   def productdetails
     {
       material_and_care: object.care_instructions,
-      size_and_fit: object.size_and_fit + " " + object.height,
+      size_and_fit: object.size_and_fit.to_s + " " + object.height.to_s,
       description: object.description
     }
   end
