84e191657df25857247c2cb5aa63dff0a938a41c
manojverma12345 <manoj.verma@deeporion.com>
2024-10-07 17:05:08 +0530

    wrok on bug

--- app/models/product_item_variant.rb
+++ app/models/product_item_variant.rb
@@ -5,5 +5,15 @@ class ProductItemVariant < ApplicationRecord
   
   validates :size, presence: true, uniqueness: { scope: :product_item_id }, on: :create
   validates :quantity, presence: true, on: :create
+
+  after_create :set_discounted_price
+
+  private
+
+  def set_discounted_price
+    if discounted_price.nil?
+      update(discounted_price: price)
+    end
+  end
 end
 
