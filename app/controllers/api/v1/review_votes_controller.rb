2a1b8cbc7f393a1c66eb430f530e245ba8b3ff59
manojverma12345 <manoj.verma@deeporion.com>
2024-11-26 15:17:17 +0530

    work on reviewvotes

--- app/controllers/api/v1/review_votes_controller.rb
+++ app/controllers/api/v1/review_votes_controller.rb
@@ -5,15 +5,22 @@ class Api::V1::ReviewVotesController < ApplicationController
     @review = Review.find(params[:review_id])
     @review_vote = @review.review_votes.find_or_initialize_by(user: @current_user)
 
-    if @review_vote.update(review_vote_params)
+    if @review_vote.persisted? && @review_vote.helpful == params[:helpful]
+      @review_vote.destroy
       helpful_true_count = @review.review_votes.where(helpful: true).count
       helpful_false_count = @review.review_votes.where(helpful: false).count
-      render json: { review_vote: @review_vote, helpful_true_count: helpful_true_count, helpful_false_count: helpful_false_count }, status: :created
+      render json: { message: 'Vote removed', helpful_true_count: helpful_true_count, helpful_false_count: helpful_false_count }, status: :ok
     else
-      render json: @review_vote.errors, status: :unprocessable_entity
+
+      if @review_vote.update(review_vote_params)
+        helpful_true_count = @review.review_votes.where(helpful: true).count
+        helpful_false_count = @review.review_votes.where(helpful: false).count
+        render json: { review_vote: @review_vote, helpful_true_count: helpful_true_count, helpful_false_count: helpful_false_count }, status: :created
+      else
+        render json: @review_vote.errors, status: :unprocessable_entity
+      end
     end
   end
-
 
   private
 
