6bd3ea5748c28fd9ba4d2b103fbb18c1b6195118
manojverma12345 <manoj.verma@deeporion.com>
2024-12-03 17:38:47 +0530

    change in category

--- app/serializers/subcategory_serializer.rb
+++ app/serializers/subcategory_serializer.rb
@@ -1,3 +1,8 @@
 class SubcategorySerializer < ActiveModel::Serializer
   attributes :id, :name, :category_id, :products
+
+  def products
+    ordered_products = object.products.order(created_at: :asc)
+    ActiveModelSerializers::SerializableResource.new(ordered_products, each_serializer: ProductSerializer)
+  end
 end
